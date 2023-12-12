55a5f584e4cdb778ab4ea28e336556f962de7005
DK <skyhome@MacBook-Pro-DK.lan>
2023-12-12 09:31:00 +0200

    finished working on the exercises

---
+++ Week3/Day2/XP-DC.sql
@@ -0,0 +1,51 @@
+-- CREATE TABLE FirstTab (
+--      id integer, 
+--      name VARCHAR(10)
+-- )
+
+-- INSERT INTO FirstTab VALUES
+-- (5,'Pawan'),
+-- (6,'Sharlee'),
+-- (7,'Krish'),
+-- (NULL,'Avtaar')
+-- SELECT * FROM FirstTab
+
+EXPECTED OUTPUT:
+
+| ind | id     |   name    |
+| 1   | 5      | Pawan     |
+| 2   | 6      | Sharlee   |
+| 3   | 7      | Pawan     |
+| 4   | null   | Avtaar    |
+
+
+-- CREATE TABLE SecondTab (
+--     id integer 
+-- )
+
+-- INSERT INTO SecondTab VALUES
+-- (5),
+-- (NULL)
+
+
+-- SELECT * FROM SecondTab
+
+EXPECTED OUTPUT:
+
+| ind | id     |   
+| 1   | 5      |
+| 2   | null   | 
+
+
+Q1:
+3
+
+Q2:
+3
+
+Q3:
+2
+
+Q4:
+3
+
